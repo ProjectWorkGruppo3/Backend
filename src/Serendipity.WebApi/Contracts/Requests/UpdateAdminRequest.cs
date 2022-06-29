3223a973cca54d363377fa2eadf2a16a45eeef4f
Saliou <gayesaliou29@gmail.com>
2022-06-29 19:51:34 +0200

    chore: add admin crud operations

--- src/Serendipity.WebApi/Contracts/Requests/UpdateAdminRequest.cs
+++ src/Serendipity.WebApi/Contracts/Requests/UpdateAdminRequest.cs
@@ -1,17 +1,12 @@
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Serendipity.WebApi.Contracts.Requests;
 
-public class RegisterAdminRequest
+public class UpdateAdminRequest
 {
     [EmailAddress(ErrorMessage = "Invalid email format.")]
     [Required(ErrorMessage = "Email is required.")]
     public string Email { get; set; }
-
-    [DataType(DataType.Password)]
-    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
-    [Required(ErrorMessage = "Password is required.")]
-    public string Password { get; set; }
 
     [Required(ErrorMessage = "Name is required.")]
     public string Name { get; set; }
